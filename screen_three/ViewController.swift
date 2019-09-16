994c720d2de493b7e09480198bc50999b6444dd9
Shawn Ng <shawncien@gmail.com>
2019-09-16 10:52:20 +0800

    Initial Commit

---
+++ screen_three/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  screen_three
+//
+//  Created by Shawn Ng on 16/9/19.
+//  Copyright © 2019 Shawn Ng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ screen_threeTests/screen_threeTests.swift
@@ -0,0 +1,34 @@
+//
+//  screen_threeTests.swift
+//  screen_threeTests
+//
+//  Created by Shawn Ng on 16/9/19.
+//  Copyright © 2019 Shawn Ng. All rights reserved.
+//
+
+import XCTest
+@testable import screen_three
+
+class screen_threeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
